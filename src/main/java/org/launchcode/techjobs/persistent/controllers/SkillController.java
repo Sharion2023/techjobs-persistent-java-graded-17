5944cc528350a0294b4290b87abac22873b7d9c0
Sharion2023 <Sharion06@gmail.com>
2023-11-01 09:05:02 -0500

    Task two done.

--- src/main/java/org/launchcode/techjobs/persistent/controllers/SkillController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/SkillController.java
@@ -14,7 +14,6 @@ import java.util.Optional;
 
 @Controller
 @RequestMapping("skills")
-
 public class SkillController {
 
     @Autowired
@@ -22,7 +21,7 @@ public class SkillController {
 
     @GetMapping ("/")
     public String index(Model model){
-        model.addAttribute(skillRepository.findAll());
+        model.addAttribute("skills",skillRepository.findAll());
         return "skills/index";
     }
 

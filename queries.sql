f6a8987cf4e56277bc8c6d49361d7a0a557f63cb
Sharion2023 <Sharion06@gmail.com>
2023-11-05 20:57:05 -0600

    Task four query written. Perfoms right in SQL. Still failing test.

--- queries.sql
+++ queries.sql
@@ -5,3 +5,7 @@ SELECT name FROM employer WHERE location = "St. Louis City";
 --Part 3
 DROP TABLE job;
 --Part 4
+SELECT name from techjobs.skill
+INNER JOIN techjobs.job_skills
+on techjobs.skill.id = techjobs.job_skills.skills_id
+WHERE techjobs.job_skills.jobs_id IS NOT NULL; 
